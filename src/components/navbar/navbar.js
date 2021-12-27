d40fc94d82396f075669b9c97cbb3a21bc0516bf
nirajsharma99 <59087588+nirajsharma99@users.noreply.github.com>
2021-12-27 10:38:21 +0530

    update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { MenuItems } from './menu-items';
 import '../sass/navbar.scss';
 import Hamburger from './hamburger';
